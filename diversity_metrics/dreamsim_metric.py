83613ad0294e5430f22ed5a1f8ac834b3cd30259
piadonabauer <pia.donabauer@stud.uni-regensburg.de>
2025-06-01 23:36:18 +0200

    update dreamsim

--- diversity_metrics/dreamsim_metric.py
+++ diversity_metrics/dreamsim_metric.py
@@ -10,32 +10,34 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class DreamSimMetric:
     """
-    Computes DreamSim perceptual similarity scores between images in a folder.
+    Computes DreamSim perceptual similarity scores between images from a .npz file.
+    The .npz file must contain an array under the key 'arr_0' of shape [N, H, W, C].
 
-    Args:
-        pretrained (str or bool): DreamSim model name or True for ensemble.
+    Example call from another file in main folder structure:
+
+    from diversity_metrics.dreamsim_metric import DreamSimMetric
+
+    deramsim = DreamSimMetric()
+    mean_dist, std_dist = deramsim.compute("path/tp/npz_file.npz")
+    print("Mean DreamSim Distance:", mean_dist)
+
     """
     def __init__(self, pretrained=True):
         self.model, self.preprocess = dreamsim(pretrained=pretrained, device=device)
         self.model.eval()
 
-    def _load_tensor_image(self, image_path):
-        img = Image.open(image_path).convert("RGB")
+    def _preprocess_np_image(self, np_image):
+        img = Image.fromarray(np_image.astype(np.uint8)).convert("RGB")
         return self.preprocess(img).to(device)
 
-    def compute_diversity(self, folder_path, return_pairwise_scores=False, save_scores_path=None):
-        image_files = sorted([
-            f for f in os.listdir(folder_path)
-            if f.lower().endswith(('.png', '.jpg', '.jpeg'))
-        ])
-
-        if len(image_files) < 2:
+    def compute(self, npz_path, return_pairwise_scores=False, save_scores_path=None):
+        data = np.load(npz_path)['arr_0']  # Assumes key is 'arr_0'
+        if data.shape[0] < 2:
             raise ValueError("Need at least two images to compute diversity.")
-
+        
         tensors = []
-        for f in tqdm(image_files, desc="Loading images"):
-            path = os.path.join(folder_path, f)
-            tensors.append(self._load_tensor_image(path))
+        for i in tqdm(range(data.shape[0]), desc="Loading images"):
+            tensors.append(self._preprocess_np_image(data[i]))
 
         pair_scores = {}
         distances = []
@@ -45,7 +47,7 @@ class DreamSimMetric:
                            total=len(tensors) * (len(tensors)-1) // 2):
             with torch.no_grad():
                 dist = self.model(tensors[i], tensors[j]).item()
-            pair_name = f"{image_files[i]}___{image_files[j]}"
+            pair_name = f"img_{i}___img_{j}"
             pair_scores[pair_name] = dist
             distances.append(dist)
 
